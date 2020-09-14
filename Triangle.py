b1df664ea50d6a4d64bfccb17ab4a0ccb797b6fb
BrandonSeidman <bseidman@stevens.edu>
2020-09-14 17:55:12 -0400

    Fixed Equilateral Triangle

--- Triangle.py
+++ Triangle.py
@@ -47,7 +47,7 @@ def classifyTriangle(a,b,c):
         return 'NotATriangle'
 
     # now we know that we have a valid triangle
-    if a == b and b == a:
+    if a == b and c == a and c == b:
         return 'Equilateral'
     elif ((a * 2) + (b * 2)) == (c * 2):
         return 'Right'
