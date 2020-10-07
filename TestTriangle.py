ce4e24c970b0f8de686be1287f8be7eb99eb5bb6
BrandonSeidman <bseidman@stevens.edu>
2020-10-07 15:31:40 -0400

    Updated HW05

--- TestTriangle.py
+++ TestTriangle.py
@@ -9,111 +9,186 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
+from Triangle import _classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangles(unittest.TestCase):
+_NAT = 'NotATriangle'
+_SB_NAT = 'Should be NotATriangle'
+_SB_NNAT = 'Should not be NotATriangle'
+_EQ = 'Equilateral'
+_SB_EQ = 'Should be Equilateral'
+_SB_NEQ = 'Should not be Equilateral'
+_R = 'Right'
+_SB_R = 'Should be Right'
+_SB_NR = 'Should not be Right'
+_S = 'Scalene'
+_SB_S = 'Should be Scalene'
+_SB_NS = 'Should not be Scalene'
+_I = 'Isoceles'
+_SB_I = 'Should be Isoceles'
+_SB_NI = 'Should not be Isoceles'
+
+class _TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
+    def test_not_a_triangle_1(self):
+        '''NaT1'''
+        self.assertEqual(_classify_triangle(1,2,3),_NAT,_SB_NAT)
+        #Normal Test
 
-    def testNotATriangle1(self):
-        self.assertEqual(classifyTriangle(1,2,3),'NotATriangle','Should be NotATriangle') #Normal Test
+    def test_not_a_triangle_2(self):
+        '''NaT2'''
+        self.assertEqual(_classify_triangle(2,3,1),_NAT,_SB_NAT)
+        #Tests different positions
 
-    def testNotATriangle2(self):
-        self.assertEqual(classifyTriangle(2,3,1),'NotATriangle','Should be NotATriangle') #Tests different positions
+    def test_not_a_triangle_3(self):
+        '''NaT3'''
+        self.assertNotEqual(_classify_triangle(2,2,3),_NAT,_SB_NNAT)
+        #Tests an actual triangle
 
-    def testNotATriangle3(self):
-        self.assertNotEqual(classifyTriangle(2,2,3),'NotATriangle','Should not be NotATriangle') #Tests an actual triangle
+    def test_not_a_triangle_4(self):
+        '''NaT4'''
+        self.assertNotEqual(_classify_triangle(2,3,2),_NAT,_SB_NNAT)
+    #Tests an actual triangle in different positions
 
-    def testNotATriangle4(self):
-        self.assertNotEqual(classifyTriangle(2,3,2),'NotATriangle','Should not be NotATriangle') #Tests an actual triangle in different positions
-
-    def testNotATriangle5(self):
-        self.assertEqual(classifyTriangle(10,2,89),'NotATriangle','Should be NotATriangle') #Tests with equations
-
-
-
-    def testEquilateralTriangle1(self):
-        self.assertEqual(classifyTriangle(3,3,3),'Equilateral','Should be Equilateral') #Normal Test
-
-    def testEquilateralTriangle2(self):
-        self.assertEqual(classifyTriangle(5,5,5),'Equilateral','Should be Equilateral') #Tests different numbers
-
-    def testEquilateralTriangle3(self):
-        self.assertNotEqual(classifyTriangle(3,3,2),'Equilateral','Should not be Equilateral') #Tests a non equilateral triangle (2 similar sides)
-
-    def testEquilateralTriangle4(self):
-        self.assertNotEqual(classifyTriangle(3,2,3),'Equilateral','Should not be Equilateral') #Tests a non equilateral triangle (2 similar sides different positions)
-
-    def testEquilateralTriangle5(self):
-        self.assertNotEqual(classifyTriangle(2,3,3),'Equilateral','Should not be Equilateral') #Tests a non equilateral triangle (2 similar sides different positions)
-
-    def testEquilateralTriangle6(self):
-        self.assertNotEqual(classifyTriangle(5,2,3),'Equilateral','Should not be Equilateral') #Tests no similar sides
-
-    def testEquilateralTriangle7(self):
-        self.assertEqual(classifyTriangle(12,6*2,3*4),'Equilateral','Should be Equilateral') #Tests equations
+    def test_not_a_triangle_5(self):
+        '''NaT5'''
+        self.assertEqual(_classify_triangle(10,2,89),_NAT,_SB_NAT)
+        #Tests with equations
 
 
 
-    def testRightTriangle1(self):
-        self.assertEqual(classifyTriangle(3,4,5),'Right','Should not be Right') #Normal Test
+    def test_equilateral_triangle_1(self):
+        '''E1'''
+        self.assertEqual(_classify_triangle(3,3,3),_EQ,_SB_EQ)
+        #Normal Test
 
-    def testRightTriangle2(self):
-        self.assertEqual(classifyTriangle(4,5,3),'Right','Should not be Right') #Tests different positions
+    def test_equilateral_triangle_2(self):
+        '''E2'''
+        self.assertEqual(_classify_triangle(5,5,5),_EQ,_SB_EQ)
+        #Tests different numbers
 
-    def testRightTriangle3(self):
-        self.assertNotEqual(classifyTriangle(6,4,4),'Right','Should not be Right') #Tests a non right triangle
+    def test_equilateral_triangle_3(self):
+        '''E3'''
+        self.assertNotEqual(_classify_triangle(3,3,2),_EQ,_SB_NEQ)
+        #Tests a non equilateral triangle (2 similar sides)
 
-    def testRightTriangle4(self):
-        self.assertNotEqual(classifyTriangle(4,4,6),'Right','Should not be Right') #Tests a non right triangle in different positions
+    def test_equilateral_triangle_4(self):
+        '''E4'''
+        self.assertNotEqual(_classify_triangle(3,2,3),_EQ,_SB_NEQ)
+        #Tests a non equilateral triangle (2 similar sides different positions)
 
-    def testRightTriangle5(self):
-        self.assertEqual(classifyTriangle(5*7,13*7,12*7),'Right','Should be Right') #Tests with equations
+    def test_equilateral_triangle_5(self):
+        '''E5'''
+        self.assertNotEqual(_classify_triangle(2,3,3),_EQ,_SB_NEQ)
+        #Tests a non equilateral triangle (2 similar sides different positions)
 
+    def test_equilateral_triangle_6(self):
+        '''E6'''
+        self.assertNotEqual(_classify_triangle(5,2,3),_EQ,_SB_NEQ)
+        #Tests no similar sides
 
-
-    def testScaleneTriangle1(self):
-        self.assertEqual(classifyTriangle(4,3,2),'Scalene','Should be Scalene') #Normal Test
-
-    def testScaleneTriangle2(self):
-        self.assertEqual(classifyTriangle(2,4,5),'Scalene','Should be Scalene') #Tests different numbers
-
-    def testScaleneTriangle3(self):
-        self.assertNotEqual(classifyTriangle(3,3,2),'Scalene','Should not be Scalene') #Tests a non scalene triangle
-
-    def testScaleneTriangle4(self):
-        self.assertNotEqual(classifyTriangle(3,2,3),'Scalene','Should not be Scalene') #Tests a non scalene triangle (different positions)
-
-    def testScaleneTriangle5(self):
-        self.assertNotEqual(classifyTriangle(2,3,3),'Scalene','Should not be Scalene') #Tests a non scalene triangle (different positions)
-
-    def testScaleneTriangle6(self):
-        self.assertNotEqual(classifyTriangle(3,3,3),'Scalene','Should not be Scalene') #Tests a non scalene triangle (all three same)
-
-    def testScaleneTriangle7(self):
-        self.assertEqual(classifyTriangle(4*6,3*6,2*6),'Scalene','Should be Scalene') #Tests with equations
+    def test_equilateral_triangle_7(self):
+        '''E7'''
+        self.assertEqual(_classify_triangle(12,6*2,3*4),_EQ,_SB_EQ)
+        #Tests equations
 
 
 
-    def testIsocelesTriangle1(self):
-        self.assertEqual(classifyTriangle(4,4,2),'Isoceles','Should be Isoceles') #Normal Test
+    def test_right_triangle_1(self):
+        '''R1'''
+        self.assertEqual(_classify_triangle(3,4,5),_R,_SB_R)
+        #Normal Test
 
-    def testIsocelesTriangle2(self):
-        self.assertEqual(classifyTriangle(2,5,5),'Isoceles','Should be Isoceles') #Tests different positions
+    def test_right_triangle_2(self):
+        '''R2'''
+        self.assertEqual(_classify_triangle(4,5,3),_R,_SB_R)
+        #Tests different positions
 
-    def testIsocelesTriangle3(self):
-        self.assertEqual(classifyTriangle(5,2,5),'Isoceles','Should be Isoceles') #Tests different positions
+    def test_right_triangle_3(self):
+        '''R3'''
+        self.assertNotEqual(_classify_triangle(6,4,4),_R,_SB_NR)
+        #Tests a non right triangle
 
-    def testIsocelesTriangle4(self):
-        self.assertNotEqual(classifyTriangle(3,5,2),'Isoceles','Should not be Isoceles') #Tests none the same
+    def test_right_triangle_4(self):
+        '''R4'''
+        self.assertNotEqual(_classify_triangle(4,4,6),_R,_SB_NR)
+        #Tests a non right triangle in different positions
 
-    def testIsocelesTriangle5(self):
-        self.assertNotEqual(classifyTriangle(2,2,2),'Isoceles','Should not be Isoceles') #Tests all the same
+    def test_right_triangle_5(self):
+        '''R5'''
+        self.assertEqual(_classify_triangle(5*7,13*7,12*7),_R,_SB_R)
+        #Tests with equations
 
-    def testIsocelesTriangle6(self):
-        self.assertEqual(classifyTriangle(4*1,4*1,2*1),'Isoceles','Should be Isoceles') #Tests with equations
+
+
+    def test_scalene_triangle_1(self):
+        '''S1'''
+        self.assertEqual(_classify_triangle(4,3,2),_S,_SB_S)
+        #Normal Test
+
+    def test_scalene_triangle_2(self):
+        '''S2'''
+        self.assertEqual(_classify_triangle(2,4,5),_S,_SB_S)
+        #Tests different numbers
+
+    def test_scalene_triangle_3(self):
+        '''S3'''
+        self.assertNotEqual(_classify_triangle(3,3,2),_S,_SB_NS)
+        #Tests a non scalene triangle
+
+    def test_scalene_triangle_4(self):
+        '''S4'''
+        self.assertNotEqual(_classify_triangle(3,2,3),_S,_SB_NS)
+        #Tests a non scalene triangle (different positions)
+
+    def test_scalene_triangle_5(self):
+        '''S5'''
+        self.assertNotEqual(_classify_triangle(2,3,3),_S,_SB_NS)
+        #Tests a non scalene triangle (different positions)
+
+    def test_scalene_triangle_6(self):
+        '''S6'''
+        self.assertNotEqual(_classify_triangle(3,3,3),_S,_SB_NS)
+        #Tests a non scalene triangle (all three same)
+
+    def test_scalene_triangle_7(self):
+        '''S7'''
+        self.assertEqual(_classify_triangle(4*6,3*6,2*6),_S,_SB_S)
+        #Tests with equations
+
+
+
+    def test_isoceles_triangle_1(self):
+        '''I1'''
+        self.assertEqual(_classify_triangle(4,4,2),_I,_SB_I)
+        #Normal Test
+
+    def test_isoceles_triangle_2(self):
+        '''I2'''
+        self.assertEqual(_classify_triangle(2,5,5),_I,_SB_I)
+        #Tests different positions
+
+    def test_isoceles_triangle_3(self):
+        '''I3'''
+        self.assertEqual(_classify_triangle(5,2,5),_I,_SB_I)
+        #Tests different positions
+
+    def test_isoceles_triangle_4(self):
+        '''I4'''
+        self.assertNotEqual(_classify_triangle(3,5,2),_I,_SB_NI)
+        #Tests none the same
+
+    def test_isoceles_triangle_5(self):
+        '''I5'''
+        self.assertNotEqual(_classify_triangle(2,2,2),_I,_SB_NI)
+        #Tests all the same
+
+    def test_isoceles_triangle_6(self):
+        '''I6'''
+        self.assertEqual(_classify_triangle(4*1,4*1,2*1),_I,_SB_I)
+        #Tests with equations
 
 
 if __name__ == '__main__':
--- Triangle.py
+++ Triangle.py
@@ -9,7 +9,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+def _classify_triangle(_a,_b,_c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of
     you test cases.
@@ -26,32 +26,34 @@ def classifyTriangle(a,b,c):
 
       BEWARE: there may be a bug or two in this code
     """
-
+    _in_a = isinstance(_a,int)
+    _in_b = isinstance(_b,int)
+    _in_c = isinstance(_c,int)
+    _ov_th = _a > 200 or _b > 200 or _c > 200
+    _lt_z = _a <= 0 or _b <= 0 or _c <= 0
     # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
 
     # verify that all 3 inputs are integers
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+    if not(_in_a and _in_b and _in_c) or _ov_th or _lt_z:
+        return 'InvalidInput'
 
     # This information was not in the requirements spec but
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+    if (_a >= (_b + _c)) or (_b >= (_a + _c)) or (_c >= (_a + _b)):
         return 'NotATriangle'
 
+    _a_pow = pow(_a,2)
+    _b_pow = pow(_b,2)
+    _c_pow = pow(_c,2)
+
     # now we know that we have a valid triangle
-    if a == b and c == a and c == b:
+    if _a == _b and _c == _a and _c == _b:
         return 'Equilateral'
-    elif pow(a,2) + pow(b,2) == pow(c,2) or pow(a,2) + pow(c,2) == pow(b,2) or pow(c,2) + pow(b,2) == pow(a,2):
+    if _a_pow + _b_pow == _c_pow or _a_pow + _c_pow == _b_pow or _c_pow + _b_pow == _a_pow:
         return 'Right'
-    elif (a != b) and  (b != c) and (a != c):
+    if (_a != _b) and  (_b != _c) and (_a != _c):
         return 'Scalene'
-    else:
-        return 'Isoceles'
+    return 'Isoceles'
